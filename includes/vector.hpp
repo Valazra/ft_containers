9d59c302d53cd3d990749c2f26df211bd57593f7
Azra <valentin.azra@gmail.com>
2022-05-29 16:42:33 +0200

    gogo

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -14,12 +14,12 @@ namespace ft
 			typedef Alloc allocator_type;
 	//		typedef const T* const_iterator;
 	//		typedef typename allocator_type::const_pointer const_pointer;
-	//		typedef typename allocator_type::const_reference const_reference;
+			typedef typename allocator_type::const_reference const_reference;
 	//		typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
 	//		typedef std::ptrdiff_t difference_type;
 	//		typedef T* iterator;
 			typedef typename allocator_type::pointer pointer;
-	//		typedef typename allocator_type::reference reference;
+			typedef typename allocator_type::reference reference;
 	//		typedef typename ft::reverse_iterator<iterator> reverse_iterator;
 			typedef std::size_t size_type;
 			typedef T value_type; 
@@ -28,10 +28,17 @@ namespace ft
 			{
 			}
 		
-			explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _size(n), _capacity(n), _array(NULL), _allocator(alloc)
+			explicit vector(size_type n, const value_type & val = value_type(), const allocator_type & alloc = allocator_type()) : _size(n), _capacity(n), _array(NULL), _allocator(alloc)
 			{
+				if (n > 0)
+				{
+					_array = _allocator.allocate(_capacity);
+					for (size_type i = 0 ; i < n ; i++)
+						_allocator.construct(_array + i, val);
+				}
 			}
-		
+	
+//peut etre InputOperator et pas InputIterator ?	
 		template <class InputIterator>
 			vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
 			{
@@ -39,14 +46,100 @@ namespace ft
 
 			vector(const vector& x) : _size(x._size), _capacity(x._capacity), _array(NULL), _allocator(x._allocator)
 			{
+				if (x._size > 0)
+				{
+					_array = _allocator.allocate(_capacity);
+					for (size_type i = 0, size = x._size ; i < size ; i++)
+						_allocator.construct(_array + i, x._array[i]);
+				}
 			}
 
 			virtual ~vector(void)
 			{
+				clear();
+				if (_capacity != 0)
+					_allocator.deallocate(_array, _capacity);
 			}
 
 			vector & operator=(vector const & src)
 			{
+				if (this != &src)
+				{
+					clear();
+					if (_capacity < src._size)
+					{
+						_allocator.deallocate(_array, _capacity);
+						_capacity = src._size;
+						if (_capacity > 0)
+							_array = _allocator.allocate(_capacity);
+						else
+							return (*this);
+					}
+					_size = src._size;
+					for(size_type i = 0 ; i < src._size ; i++)
+						_allocator.construct(_array + i, src._array[i]);
+				}
+				return (*this);
+			}
+
+			reference operator[](size_type n)
+			{
+				return (_array[n]);
+			}
+
+			const reference operator[](size_type n) const
+			{
+				return (_array[n]);
+			}
+			
+			bool empty(void) const
+			{
+				return (_size == 0 ? true : false);
+			}
+
+			size_type size(void) const
+			{
+				return (_size);
+			}
+
+			size_type capacity(void) const
+			{
+				return (_capacity);
+			}
+			
+			void clear(void)
+			{
+				if (empty())
+					return ;
+				for (size_type i = 0, size = _size ; i < size ; i++)
+					_allocator.destroy(_array + i);
+				_size = 0;
+			}
+
+			void reserve(size_type n)
+			{
+				if (n <= _capacity)
+					return ;
+				pointer ptr_new = _allocator.allocate(n);
+				for (size_type i = 0 ; i < _size ; i++)
+				{
+					_allocator.construct(ptr_new + i, _array[i]);
+					_allocator.destroy(_array + i);
+				}
+				if (_capacity != 0)
+					_allocator.deallocate(_array, _capacity);
+				_capacity = n;
+				_array = ptr_new;
+			}
+
+			void push_back(value_type const & val)
+			{
+				if (_capacity == 0)
+					reserve(1);
+				else if (_capacity == _size)
+					reserve(_capacity * 2);
+				_allocator.construct(_array + _size, val);
+				_size++;
 			}
 
 		private :
--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
-#include "includes/stack.hpp"
 #include "includes/vector.hpp"
+#include "includes/stack.hpp"
 #include "includes/map.hpp"
+
+template < typename T >
+void print_datas_vector(ft::vector<T> const & vect)
+{
+	std::cout << "Vector size is : " << vect.size() << " and capacity is : " << vect.capacity() << std::endl << std::endl;
+	std::cout << "Datas of vector : " << std::endl;
+	for (size_t i = 0 ; i < vect.size() ; i++)
+		std::cout << i << " : " << vect[i] << std::endl;
+	std::cout << std::endl << "******************************" << std::endl << std::endl;
+}
 
 int	main(void)
 {
+	std::cout << "***************************" << std::endl << std::endl;
+
+	//default constructor
+	ft::vector<int> cons1;
+	print_datas_vector(cons1);
+	cons1.push_back(5);
+	cons1.push_back(10);
+	cons1.push_back(15);
+	cons1.push_back(15);
+	cons1.push_back(15);
+	print_datas_vector(cons1);
+
+	//copy constructor
+	ft::vector<int> cons2(cons1);
+	print_datas_vector(cons2);
+	
 	return (0);
 }
